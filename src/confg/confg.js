2c29f98688a9ed653e952755d1b46d2269728d22
Suhail Shaikh <suhailshk333@gmail.com>
2023-11-14 22:27:36 +0530

    ok

--- src/confg/confg.js
+++ src/confg/confg.js
@@ -1,8 +1,8 @@
 const confg = {
-    appWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
-    appWriteprojectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appWriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appWriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appWriteUrl:String(import.meta.env.VITEAPPWRITEURL),
+    appWriteprojectId:String(import.meta.env.VITEAPPWRITEPROJECTID),
+    appWriteDatabaseId:String(import.meta.env.VITEAPPWRITEDATABASEID),
+    appWriteCollectionId:String(import.meta.env.VITEAPPWRITECOLLECTIONID),
+    appWriteBucketId:String(import.meta.env.VITEAPPWRITEBUCKETID)
 }
 export default confg;
